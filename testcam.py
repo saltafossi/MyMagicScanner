91c35360cfe4b01b0bc25b94c30f7525c7174fdb
f.matteucci@cineca.it <f.matteucci@cineca.it>
2023-06-19 11:15:20 +0200

    wip

--- testcam.py
+++ testcam.py
@@ -1,23 +1,47 @@
-import cv2
+import pygame
+import pygame.camera
 
-# Initialize the USB camera
-camera = cv2.VideoCapture(0)  # Use index 0 for the first USB camera
+# Initialize Pygame and the camera module
+pygame.init()
+pygame.camera.init()
 
-# Check if the camera is opened successfully
-if not camera.isOpened():
-    print("Failed to open the camera")
+# Get a list of available cameras
+cam_list = pygame.camera.list_cameras()
+
+if not cam_list:
+    print("No cameras found.")
     exit()
 
-# Capture a frame from the camera
-ret, frame = camera.read()
+# Create a camera object
+cam = pygame.camera.Camera(cam_list[0])
 
-# Check if the frame was captured successfully
-if not ret:
-    print("Failed to capture frame")
-    exit()
+# Start the camera
+cam.start()
 
-# Save the captured frame as an image file
-cv2.imwrite('image.jpg', frame)
+# Set the desired resolution
+width, height = 640, 480
+cam.set_resolution(width, height)
 
-# Release the camera resources
-camera.release()
+# Initialize the Pygame display
+screen = pygame.display.set_mode((width, height))
+
+# Main loop for capturing and displaying frames
+running = True
+while running:
+    # Capture a frame from the camera
+    image = cam.get_image()
+
+    # Display the frame on the screen
+    screen.blit(image, (0, 0))
+    pygame.display.flip()
+
+    # Check for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# Stop the camera
+cam.stop()
+
+# Quit Pygame
+pygame.quit()
