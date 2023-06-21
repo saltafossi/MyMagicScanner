2956b530343a46b101817e318f6bad5b4c6e8a42
f.matteucci@cineca.it <f.matteucci@cineca.it>
2023-06-21 11:55:02 +0200

    wip

---
+++ stopservo.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin used for the servo
+servo_pin = 24
+
+# Set up the GPIO pin for output
+GPIO.setup(servo_pin, GPIO.OUT)
+
+# Create a PWM instance with frequency 50Hz
+pwm = GPIO.PWM(servo_pin, 50)
+
+pwm.stop()
+GPIO.cleanup()
