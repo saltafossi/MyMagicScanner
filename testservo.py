a7fe8f00a00677f04179476d5bb4beb5177abb91
f.matteucci@cineca.it <f.matteucci@cineca.it>
2024-01-03 12:59:25 +0100

    w

--- testservo.py
+++ testservo.py
@@ -10,9 +10,11 @@ p.start(0) # Initialization
 time.sleep(2)
 try:
   while True:
-    p.ChangeDutyCycle(13.5)
+    p.ChangeDutyCycle(5)
     time.sleep(3)
-    p.ChangeDutyCycle(6.7)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(3)
+    p.ChangeDutyCycle(10)
     time.sleep(3)
 except KeyboardInterrupt:
   time.sleep(0.5)
