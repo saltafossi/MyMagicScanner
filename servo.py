17be0a4b7a0a402ad6fe29a972740fb3fd980f1a
f.matteucci@cineca.it <f.matteucci@cineca.it>
2024-01-03 13:05:59 +0100

    w

--- servo.py
+++ servo.py
@@ -10,8 +10,6 @@ args = sys.argv
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 24 for PWM with 50Hz
 p.start(0) # Initialization
-time.sleep(2)
 p.ChangeDutyCycle(float(args[1]))
-time.sleep(3)
 p.stop()
 GPIO.cleanup()
