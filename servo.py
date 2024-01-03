a9597bd4c68f921ea27c30383321ccb58dd0febb
f.matteucci@cineca.it <f.matteucci@cineca.it>
2024-01-03 13:03:18 +0100

    w

---
+++ servo.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+servoPIN = 24
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+args = sys.argv
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 24 for PWM with 50Hz
+p.start(0) # Initialization
+time.sleep(2)
+p.ChangeDutyCycle(args[1])
+time.sleep(3)
+p.stop()
+GPIO.cleanup()
